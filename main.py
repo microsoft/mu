6d93efcb18190fe856593f4a6a7718895ee717d1
Sean Brogan <spbrogan@live.com>
2018-09-27 20:52:01 -0700

    Add main.py to allow for dynamic values during the build of the docs.  This will enable adding the git commit hash and branch of each downstream doc. (#2)

---
+++ main.py
@@ -0,0 +1,42 @@
+#
+# This uses the macro plugin 
+# https://github.com/fralau/mkdocs_macros_plugin
+#
+# To install use pip install mkdocs-macros-plugin
+#
+
+def declare_variables(variables, macro):
+    """
+    This is the hook for the functions
+
+    - variables: the dictionary that contains the variables
+    - macro: a decorator function, to declare a macro.
+    """
+
+    variables['version'] = "0.3"
+
+    import datetime
+    @macro
+    def buildtime():
+      return str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+
+''' 
+
+SAMPLE OF HOW TO USE MACROS.
+    @macro
+    def bar(x):
+        return (2.3 * x) + 7
+
+
+
+    # If you wish, you can  declare a macro with a different name:
+    def f(x):
+        return x * x
+
+    macro(f, 'barbaz')
+
+    # or to export some predefined function
+    import math
+    macro(math.floor) # will be exported as 'floor'
+
+'''
