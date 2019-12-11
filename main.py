2675061c096b3ed989119deb2ef72cd62a530e12
Sean Brogan <spbrogan@live.com>
2019-12-10 18:42:00 -0800

    Add noopt main.py module to enable newer plugin version (#82)
    * Add noopt main.py module to enable newer plugin version

---
+++ main.py
@@ -0,0 +1,12 @@
+def define_env(env):
+    """
+    This is the hook for defining variables, macros and filters
+
+    - variables: the dictionary that contains the environment variables
+    - macro: a decorator function, to declare a macro.
+    """
+
+    # Due to what seems like a bug in mkdocs-macros-plugin this module must exist with this
+    # function.  I have opened a bug https://github.com/fralau/mkdocs_macros_plugin/issues/16
+    # and hopefully in the future this module can be removed.
+    pass
